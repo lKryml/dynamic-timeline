9fee076a1219e3dc167f95b5f318a7544f98784c
Krym <gumimash@gmail.com>
2024-01-06 22:23:45 +0200

    init

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+const Timeline = () => {
+    return (
+        <div>Timeline</div>
+    )
+}
+
+const Circle = () => {
+    return (
+        <div className="rounded-full w-4 h-4 bg-blue-500 mx-auto">
+
+        </div>
+    )
+}
+
+export default Timeline;
